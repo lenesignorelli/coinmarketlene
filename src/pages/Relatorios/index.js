f49a92e8518cee80ce9aa8904476625a2de654d8
Lene Signorelli <lenesignorelli@yahoo.com.br>
2023-03-19 00:41:45 -0300

    Complementa paginas de relatorios

--- src/pages/Relatorios/index.js
+++ src/pages/Relatorios/index.js
@@ -1,9 +1,39 @@
 import './styles.css'
 
+const reports = [
+  {
+    codigo: 100, titulo: 'Qui eu id quis', 
+    texto: 'Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.',
+  },
+  {
+    codigo: 200, titulo: "Qui eu id quis", 
+    texto: 'Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.',
+  },
+  {
+    codigo: 300, titulo: "Qui eu id quis", 
+    texto: 'Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.Qui eu id quis occaecat aliquip anim duis nisi eiusmod velit non sint.',
+  },
+];
+
 export default function Relatorios() {
   return (
-    <div className='titulo-relatorios'>
-        <h1>componente de relatorios</h1>
+    <>
+    <h1 className='titulo-relatorios'>
+        Relatórios de Criptomoedas
+    </h1>
+    <div>
+      {
+        reports.map(relat => {
+          return (
+            <div className='relatorio-cartao' key={relat.codigo}>
+              <span className='sp-codigo'>{relat.codigo}</span>
+              <span className='sp-titulo'>{relat.titulo}</span>
+              <span className='sp-texto'>{relat.texto}</span>
+            </div>
+          )
+        })
+      }
     </div>
+    </>
   )
 }
