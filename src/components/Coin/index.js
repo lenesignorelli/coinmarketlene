8e2586534d90e0a81338ce9abf5fc5b440d98d9a
Lene Signorelli <lenesignorelli@yahoo.com.br>
2023-03-18 22:26:41 -0300

    Implementa componentes Cotacoes e Coin

---
+++ src/components/Coin/index.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import './styles.css';
+
+//props
+function Coin({ id, nome, preco, variacao}) {
+  return (
+    <tr key={id}>
+      <td>{id}</td>
+      <td>{nome}</td>
+      <td>{preco}</td>
+      <td>{variacao}</td>
+    </tr>
+  )
+}
+
+Coin.propTypes = {
+  id: PropTypes.number.isRequired,
+  nome: PropTypes.string.isRequired,
+  preco: PropTypes.number.isRequired,
+  variacao: PropTypes.number.isRequired
+}
+
+export default Coin;
--- src/pages/Cotacoes/index.js
+++ src/pages/Cotacoes/index.js
@@ -1,17 +1,31 @@
+import Coin from '../../components/Coin';
 import './styles.css';
+
+const criptos = [
+    { "id": 1, "sigla": "BTC", "nome": "BitCoin", "preco": 115000.25, "variacaoPerc": 3.55 }, 
+    { "id": 2, "sigla": "ETH", "nome": "Ethereum", "preco": 8114.03, "variacaoPerc": 1.94 },
+    { "id": 3, "sigla": "USDC", "nome": "USDCoin", "preco": 5.09, "variacaoPerc": -0.15 },
+    { "id": 4, "sigla": "LTC", "nome": "LiteCoin", "preco": 447.33, "variacaoPerc": 1.99 } 
+];
 
 export default function Cotacoes() {
   return (
-    <table>
+    <table className='tabela-cotacoes'>
         <thead>
             <tr>
-                <thead>id</thead> 
-                <thead>Moeda</thead>
-                <thead>Preço</thead>
-                <thead>Variação</thead>               
+                <th>id</th> 
+                <th>Moeda</th>
+                <th>Preço</th>
+                <th>Variação</th>               
             </tr>
         </thead>
-        Componente de cotações
+        <tbody>
+            {
+                criptos.map(coin => <Coin key={coin.id}
+                    id={coin.id} sigla={coin.sigla} nome={coin.nome} preco={coin.preco}
+                    variacao={coin.variacao} simbolo={coin.simbolo} />)
+           }
+        </tbody>
     </table>
   )
 }
